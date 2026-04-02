648454d516ca21b86dd6e7f12c44fba083fa7fc9
Rod <aaronrodolfo2022@gmail.com>
2026-04-02 17:18:57 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//11 = x + 5 - an arithmetic operation for this lesson
+
+
+
+let coffee = 30;
+
+coffee -=10;
+let bread = 50
+
+let matches = coffee - bread;
+
+console.log(`the price for the mix and match is ${matches}`);
+
+matches +=50;
+
+
+document.getElementById(`info`).textContent = (`the price for the mix and match is ${matches}`);
