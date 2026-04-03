f5498c54410032453bd4b333c236ab3be12c3e1c
Rod <aaronrodolfo2022@gmail.com>
2026-04-03 18:08:32 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-//11 = x + 5 - an arithmetic operation for this lesson
+//a project !!
 
 
+let a = "Rod!";
 
-let coffee = 30;
+document.getElementById(`myh1`).textContent = (`Welcome to the website, ${a}!`);
 
-coffee -=10;
-let bread = 50
+let b = "The datas for each students with their grades at school:";
 
-let matches = coffee - bread;
+document.getElementById(`mypre`).textContent = (`${b}`);
 
-console.log(`the price for the mix and match is ${matches}`);
+let c;
+c = 96;
 
-matches +=50;
+document.getElementById(`p1`).textContent = (`This is the student a's grade: ${c}.`);
 
+let d;
+d = 94;
 
-document.getElementById(`info`).textContent = (`the price for the mix and match is ${matches}`);
+document.getElementById(`p2`).textContent = (`This is the student b's grade: ${d}.`);
+
+let e;
+e = 98;
+
+document.getElementById(`p3`).textContent = (`This is the student c's grade: ${e}`);
+
+let f;
+f = 97;
+
+document.getElementById(`p4`).textContent = (`This is the student d's grade: ${f}.`);
+
+let g;
+g = 96;
+
+document.getElementById(`p5`).textContent = (`This is the student e's grade: ${g}.`);
+
+let h = false;
+
+document.getElementById(`myh2`).textContent = (`The highest grade is 98% obtained by the student d: ${h}`);
