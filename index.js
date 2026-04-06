084d0589db6243da129f4e22826b1ce0ab1d0437
Rod <aaronrodolfo2022@gmail.com>
2026-04-06 17:32:47 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,7 @@
-//a project !!
+//window prompt
 
 
-let a = "Rod!";
+let y = window.prompt("What's your username??");
 
-document.getElementById(`myh1`).textContent = (`Welcome to the website, ${a}!`);
 
-let b = "The datas for each students with their grades at school:";
-
-document.getElementById(`mypre`).textContent = (`${b}`);
-
-let c;
-c = 96;
-
-document.getElementById(`p1`).textContent = (`This is the student a's grade: ${c}.`);
-
-let d;
-d = 94;
-
-document.getElementById(`p2`).textContent = (`This is the student b's grade: ${d}.`);
-
-let e;
-e = 98;
-
-document.getElementById(`p3`).textContent = (`This is the student c's grade: ${e}`);
-
-let f;
-f = 97;
-
-document.getElementById(`p4`).textContent = (`This is the student d's grade: ${f}.`);
-
-let g;
-g = 96;
-
-document.getElementById(`p5`).textContent = (`This is the student e's grade: ${g}.`);
-
-let h = false;
-
-document.getElementById(`myh2`).textContent = (`The highest grade is 98% obtained by the student d: ${h}`);
+document.getElementById(`myH1`).textContent = (`WELCOME, ${y}!`);
