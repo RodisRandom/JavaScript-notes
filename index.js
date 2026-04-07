d9be07fd82985e866b31a8478ce5f5412c5e4278
Rod <aaronrodolfo2022@gmail.com>
2026-04-07 17:12:16 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-//window prompt
+//gallery project...
 
+let username;
+username = window.prompt(`What's your username??`);
 
-let y = window.prompt("What's your username??");
+document.getElementById(`myh1`).textContent = (`WELCOME, ${username}!`);
 
+let roblox1;
+roblox1 = window.prompt(`What's your age?`);
 
-document.getElementById(`myH1`).textContent = (`WELCOME, ${y}!`);
+document.getElementById(`description1`).textContent = (`Your age is ${roblox1}`);
+
+let roblox2;
+roblox2 = window.prompt(`What's your grade?`);
+
+document.getElementById(`description2`).textContent = (`Your grade is ${roblox2}`);
+
+let roblox3;
+roblox3 = window.prompt(`What's your height?`);
+
+document.getElementById(`description3`).textContent = (`Your height is ${roblox3}`);
