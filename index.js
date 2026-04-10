468971e0c601eaee902f8d1ab074ead1ca145378
Rod <aaronrodolfo2022@gmail.com>
2026-04-10 17:28:29 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
-//a test
-
-let a = window.prompt(`what do you call a food with crust?`);
-a = String(a);
-
-let b = window.prompt(`How expensive is a mansion house?`);
-b = Number(b);
-b+=20000;
-
-let c = window.prompt(`Add your age to 15?`);
-c = Number(c);
-c+=15;
-
-let d = window.prompt(`Is dog always dangerous than humane? (true or false)?`);
-d = Boolean(d);
+//sadsadsada
 
 
-document.getElementById(`p1`).textContent = (`a ${a}`);
-document.getElementById(`p2`).textContent = (`more than ${b}`);
+let a = window.prompt(`What do you call a geologic feature which causes mountain ranges?`);
+a = String(`1. ${a}`);     
+
+document.getElementById(`p1`).textContent = (`${a}`);
+
+let b = "Correct Answer: DIVERGENT BOUNDARY";
+b = String(b);
+
+document.getElementById(`p2`).textContent = (`${b}`);
+
+let c = window.prompt(`What do you call an organ system in which it secretes hormones?`);
+c = String(`2. ${c}`);
+
 document.getElementById(`p3`).textContent = (`${c}`);
+
+let d = "Correct Answer: ENDOCRINE SYSTEM";
+d = String(d);
+
 document.getElementById(`p4`).textContent = (`${d}`);
+
+let e = window.prompt(`We all know there are many types of diversity, and which of those consist of falls in the environment?`);
+e = String(`3. ${e}`);
+
+document.getElementById(`p5`).textContent = (`${e}`);
+
+let f = "Correct Answer: BIODIVERSITY FALL";
+f = String(f);
+
+document.getElementById(`p6`).textContent = (`${f}`);
+
+let g = window.prompt(`Why food rotten, in what theory?`);
+g = String(`4. ${g}`);
+
+document.getElementById(`p7`).textContent = (`${g}`);
+
+let h = "Correct Answer: GERM THEORY";
+h = String(h);
+
+document.getElementById(`p8`).textContent = (`${h}`);
+
+let i = window.prompt(`What causes photosynthesis, in what parts of the botanist?`);
+i = String(`5. ${i}`);
+
+document.getElementById(`p9`).textContent = (`${i}`);
+
+let j = "Correct Answer: LEAVES";
+j = String(j);
+
+document.getElementById(`p10`).textContent = (`${j}`);
