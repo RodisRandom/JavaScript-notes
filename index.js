9866725542693384fa2aefed1c28dc63ca67303d
Rod <aaronrodolfo2022@gmail.com>
2026-04-23 17:25:49 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
 //rng
 
-
-const myButton = document.getElementById(`myButton`);
-const myLabel1 = document.getElementById(`myLabel1`);
-const myLabel2 = document.getElementById(`myLabel2`);
-const a = 1000;
-const b = 500;
+const button1 = document.getElementById(`button1`);
+const myLabel = document.getElementById(`myLabel`);
+const min = 1;
+const max = 100;
 let randomNumber;
-let randomNumber2;
+
+button1.onclick = function(){
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    myLabel.textContent = randomNumber;
+}
 
 
-myButton.onclick = function(){
-    randomNumber = Math.floor(Math.random() * (a - b)) + b;
-    randomNumber2 = Math.floor(Math.random() * (a - b)) + b;
-    myLabel1.textContent = randomNumber;
-    myLabel2.textContent = randomNumber2;
+const myLabel2 = document.getElementById(`myLabel2`);
+const button2 = document.getElementById(`button2`);
+const button3 = document.getElementById(`button3`);
+const button4 = document.getElementById(`button4`);
+const button5 = document.getElementById(`button5`);
+const button6 = document.getElementById(`button6`);
+const button7 = document.getElementById(`button7`);
+let number;
+
+button2.onclick = function(){
+    number = 0;
+    myLabel2.textContent = number;
+}
+
+button3.onclick = function(){
+    number = 20;
+    myLabel2.textContent = number;
+}
+
+button4.onclick = function(){
+    number = 40;
+    myLabel2.textContent = number;
+}
+
+button5.onclick = function(){
+    number = 60;
+    myLabel2.textContent = number;
+}
+
+button6.onclick = function(){
+    number = 80;
+    myLabel2.textContent = number;
+}
+
+button7.onclick = function(){
+    number = 100;
+    myLabel2.textContent = number;
+}
+
+button8.onclick = function(){
+    number--;
+    myLabel2.textContent = number;
+}
+
+button9.onclick = function(){
+    number++;
+    myLabel2.textContent = number;
 }
